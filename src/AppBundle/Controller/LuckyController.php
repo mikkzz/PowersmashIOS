c215e6fdc1c154e33e1551c48d9d0b4af9a13cef
Adrian King Legaspi <aki.legaspi@gmail.com>
2015-07-15 16:44:21 +0800

    another

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -1,18 +1,21 @@
 <?php
-
 namespace AppBundle\Controller;
 
-use Symfony\BundleFrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController
 {
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = rand(0, 100);
 
-  public function numberAction()
-  {
-    $number = rand(0, 100);
-    return new Response('<html><body>Lucky Number:'.$number.'</body></html>');
-  }
-
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
 }
