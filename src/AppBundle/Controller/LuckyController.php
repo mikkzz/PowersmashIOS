66f6a80f97c0a366af5ef3650febd36774deb206
Adrian King Legaspi <aki.legaspi@gmail.com>
2015-07-15 15:49:46 +0800

    Added test controller

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,15 @@
+namespace AppBundle\Controller;
+use Symfony\BundleFrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+
+  public function numberAction()
+  {
+    $number = rand(0, 100);
+    return new Response('<html><body>Lucky Number:'.$number.'</body></html>');
+  }
+
+}
