bcf42404e460dc3afbc1d0b0ab7bf56505df0337
Adrian King Legaspi <aki.legaspi@gmail.com>
2015-07-15 15:42:14 +0800

    Remove syntax error

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,7 +11,7 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR']) || php_sapi_name() === 'cli-server')
-) {
+     {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
